ada6cbd1abcea83cb8a6a7f4f67290e1bf96a8dc
Tiago Barbosa <tiagocastrobarbosa@gmail.com>
2021-11-16 06:42:39 -0300

    Separating responsibilities

--- src/FirstSteps.java
+++ src/FirstSteps.java
@@ -1,23 +1,36 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstSteps {
 
-    @Test
-    public void meuPrimeiroTest () {
+    WebDriver driver;
+
+    @Before
+    public void preCondicao() {
         // define o local do driver
         System.setProperty("webdriver.gecko.driver", "C:/Users/tiago/Documents/Programming/Java/Web/geckodriver-v0.30.0-win64/geckodriver.exe");
         // inicia o Firefox
-        WebDriver driver = new FirefoxDriver();
+        driver = new FirefoxDriver();
         // acessa a página do Google
         driver.get("http://www.google.com.br");
         // maximiza a tela
         driver.manage().window().maximize();
+    }
+
+    @Test
+    public void meuPrimeiroTest () {
         // imprime o título da página no console
         System.out.println(driver.getTitle());
         // imprime a URL atual no console
         System.out.println(driver.getCurrentUrl());
+    }
+
+    @After
+    public void posCondicao() {
         // fecha a janela do navegador
         driver.quit();
     }
--- src/JUnitAnotations.java
+++ src/JUnitAnotations.java
@@ -6,18 +6,22 @@ public class JUnitAnotations {
     public static void antesDaClasse() {
         System.out.println("Esse é o beforeClass");
     }
+
     @Before
     public void antesDosMetodos() {
         System.out.println("Esse é o before");
     }
+
     @Test
     public void meuTeste1() {
         System.out.println("Esse é o meuTeste1");
     }
+
     @Test
     public void meuTeste2() {
         System.out.println("Esse é o meuTeste2");
     }
+
     @After
     public void depoisDosMetodos() {
         System.out.println("Esse é o after");
