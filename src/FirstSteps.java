b82eec9e1a6cc682ccfcebe2fa31963e8511a776
Tiago Barbosa <tiagocastrobarbosa@gmail.com>
2021-11-16 13:31:33 -0300

    Added logic of search element by name

--- src/FirstSteps.java
+++ src/FirstSteps.java
@@ -2,7 +2,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstSteps {
@@ -23,6 +25,11 @@ public class FirstSteps {
 
     @Test
     public void myFirstTest () {
+        // procura por um elemento de locator name igual à "q"
+        WebElement searchBox = driver.findElement(By.name("q"));
+        // imprime o nome da tag HTML
+        System.out.println(searchBox.getTagName()); // -> input
+
         // imprime o título da página no console
         System.out.println(driver.getTitle());
         // imprime a URL atual no console
